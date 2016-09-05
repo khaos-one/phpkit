213e8a198834de9dff6ac13221adec9f213dd42c
Егор Зеленский <i@khaos.su>
2016-09-05 13:44:47 +0300

    Minor forms code cleaning

--- forms.php
+++ forms.php
@@ -38,9 +38,11 @@ class Form_Field {
 		$this->Required = $required;
 		$this->Errors = 0;
 	}
+
 	public function Load_Post() {
 		$this->Value = Input::Post($this->Name);
 	}
+
 	public function Validate() {
 		if (!isset($this->Value)) {
 			if ($this->Required) {
@@ -48,7 +50,9 @@ class Form_Field {
 			}
 			return !$this->Required;
 		}
+
 		$result = true;
+		
 		if (isset($this->Validation_Fn) && is_callable($this->Validation_Fn)) {
 			$res = call_user_func($this->Validation_Fn);
 			if (!$res) {
@@ -91,34 +95,43 @@ class Form {
 	private $equals;
 	private $custom_validators;
 	private $Errors;
+
 	public function __construct() {
 		$this->fields = array();
 		$this->equals = array();
 		$this->custom_validators = array();
 		$this->Errors = array();
 	}
+
 	public function Field(Form_Field $field) {
 		$this->fields[$field->Name] = $field;
 	} 
+
 	public function Load_Post() {
 		foreach ($this->fields as $field) {
 			$field->Load_Post();
 		}
 	}
+
 	public function Must_Equal($field1, $field2) {
 		$this->equals[] = array($field1->Name, $field2->Name);
 	}
+
 	public function Custom_Validator($name, $fn, $args = null) {
 		$this->custom_validators[$name] = array($fn, $args);
 	}
+
 	public function Error($name, $value = true) {
 		$this->Errors[$name] = $value;
 	}
+
 	public function Have_Errors() {
 		return !empty($this->Errors);
 	}
+
 	public function Validate() {
 		$result = true;
+
 		foreach ($this->fields as $field) {
 			$res = $field->Validate();
 			if (!$res) {
@@ -126,6 +139,7 @@ class Form {
 			}
 			$result = $result & $res;
 		}
+
 		foreach ($this->equals as $v) {
 			if (isset($this->fields[$v[0]]) && isset($this->fields[$v[1]])) {
 				if ($this->fields[$v[0]]->Value !== $this->fields[$v[1]]->Value) {
@@ -143,6 +157,7 @@ class Form {
 				}
 			}
 		}
+
 		foreach ($this->custom_validators as $k => $v) {
 			$ret = call_user_func_array($v[0], array_merge(array($this), $v[1]));
 			if (!$ret) {
@@ -152,13 +167,17 @@ class Form {
 		}
 		return $result;
 	}
+
 	public function Values() {
 		$result = array();
+
 		foreach ($this->fields as $field) {
 			$result[$field->Name] = $field->Value;
 		}
+
 		return $result;
 	}
+
 	public function Errors() {
 		return $this->Errors;
 	}
